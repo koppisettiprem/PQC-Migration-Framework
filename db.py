e0ad7a27831972588b5ef516efdbbe57a63f8c5e
koppisettiprem <koppisettiprem@gmail.com>
2026-03-25 00:49:07 +0530

    Fix database port issue

--- db.py
+++ db.py
@@ -7,6 +7,7 @@ load_dotenv()
 def get_db_connection():
     return mysql.connector.connect(
         host=os.environ.get("DB_HOST", "localhost"),
+        port=int(os.environ.get("DB_PORT", 3306)),
         user=os.environ.get("DB_USER", "root"),
         password=os.environ.get("DB_PASSWORD", ""),
         database=os.environ.get("DB_NAME", "pqc_framework")
